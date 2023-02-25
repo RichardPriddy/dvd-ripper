d8da7fd3867c3b1ac58aae0dcc2d046ce0bab958
Richard priddy <richard@thepriddyhouse.co.uk>
2023-02-25 17:25:55 +0000

    Fix everything
    I've replaced VLC and everything else because ChatGTP doesn't know what it's talking about.

--- rip-dvd.ps1
+++ rip-dvd.ps1
@@ -2,46 +2,28 @@
 $wmp = New-Object -ComObject "WMPlayer.OCX.7"
 
 # Specify the output directory for the transcoded videos
-$outputDir = "D:\Rip"
-
+$outputDir = "Z:\Movies"
+$drive = $wmp.cdromCollection.Item(0)
 # Loop indefinitely
 while ($true) {
-    # Wait for a DVD to be inserted
-    Write-Host "Waiting for DVD..."
-    while ($true) {
-        # Check for a DVD
-        $diskLabel = $null
-        foreach ($drive in $wmp.cdromCollection) {
-            if ($drive.IsMediaLoaded) {
-                $diskLabel = $drive.VolumeName
-                break
-            }
-        }
-        
-        # If a DVD is found, break out of the loop
-        if ($diskLabel) {
-            break
-        }
-        
-        # Wait a bit before checking again
-        Start-Sleep -Seconds 5
-    }
+    $diskLabel = Read-Host "DVD Title"
     
     # Clean up the DVD title for use in the output path
-    $dvdTitle = $diskLabel -replace '[\\/]','_' -replace ':',' -'
-    
-    # Build the command line arguments for VLC
-    $vlcArgs = "-I dummy"  # run VLC in dummy interface mode
-    $vlcArgs += " --no-nav"  # disable DVD menus
-    $vlcArgs += " --dvd-device $($drive.cdromcollection.item(0).driveletter) dvd://1"  # specify the DVD drive and title to rip
-    $vlcArgs += " --sout " # Transcode options
-    $vlcArgs += " :file{dst=`"$outputDir\$dvdTitle.mp4`"}"  # specify the output file path for the transcoded video
-    $vlcArgs += " --no-sout-subtitles"  # disable built-in subtitle transcoding
-    $vlcArgs += " --add-sout-subtitle=eng:file{dst=`"$outputDir\$dvdTitle.srt`"}"  # extract and save the English subtitle track
-    
+    $dvdTitle = $diskLabel -replace '\s','_' -replace ':',' -'
+
+    $makeMKV = "C:\Program Files (x86)\MakeMKV\makemkvcon64.exe"
+
+    New-Item -Path $outputDir -Name $diskLabel -ItemType "directory"
+
+    $makeMKVArgs = "mkv disc:0 all --minlength=3000 $outputDir\$diskLabel"
+
     # Transcode the DVD to an MP4 using VLC
     Write-Host "Ripping DVD: $diskLabel"
-    Start-Process "C:\Program Files (x86)\VideoLAN\VLC\vlc.exe" -ArgumentList $vlcArgs -Wait
+
+    & "C:\Program Files (x86)\MakeMKV\makemkvcon64.exe" mkv disc:0 all --minlength=3000 "$outputDir\$diskLabel"
+
+    $child = Get-ChildItem "$outputDir\$diskLabel"
+    Move-Item "$outputDir\$diskLabel\$($child[0])" "$outputDir\$diskLabel\$dvdTitle.mkv"
     
     # Eject the DVD
     $drive.Eject()
